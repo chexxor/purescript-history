060a4708bdfbbd3f09f3ddac80314210eff69adf
chexxor <chexxor@gmail.com>
2017-01-18 18:44:25 -0600

    Rewrite as simple dom history.

--- src/History.purs
+++ src/History.purs
@@ -1,60 +1,21 @@
-module History
-  ( getState
-  , pushState
-  , replaceState
+module DOM.HTML.Window.History where
 
-  , goBack
-  , goForward
-  , goState
+import Control.Monad.Eff (Eff)
+import DOM.HTML.Types (Window)
+import Prelude (Unit)
 
-  , subscribeStateChange
+foreign import data HISTORY :: !
+foreign import data History :: *
 
-  ) where
+-- DocumentTitle will set value of `document.title`
+newtype DocumentTitle = DocumentTitle String
+newtype Delta = Delta Int
+newtype URL = URL String -- Unsure how to better type this.
 
-import History.Raw
-import Data.Foreign.EasyFFI
-import Control.Monad.Eff
-import Control.Reactive
-import Control.Reactive.Event
-import Control.Apply((*>))
-
-statechange :: String
-statechange = "statechange"
-
-(<%>) :: forall a b c eff. (a -> Eff eff b) -> (a -> Eff eff c) -> a -> Eff eff c
-(<%>) x y a = x a *> y a
-
-emitStateChange s = emit $ newEvent statechange { state : s }
-
-getData :: forall eff d. Eff (history :: History d | eff) d
-getData = unsafeForeignFunction [""] "window.history.state"
-
-getTitle :: forall eff d. Eff (history :: History d | eff) Title
-getTitle = unsafeForeignFunction [""] "document.title"
-
-getUrl :: forall eff d. Eff (history :: History d | eff) Url
-getUrl = unsafeForeignFunction [""] "location.pathname"
-
-getState :: forall eff d. Eff (history :: History d | eff) (State d)
-getState = mkState <$> getData <*> getTitle <*> getUrl
-
-pushState :: forall d eff. State d -> Eff (reactive :: Reactive, history :: History d | eff) Unit
-pushState = emitStateChange <%> pushStateRaw
-
-replaceState :: forall d eff. State d -> Eff (reactive :: Reactive, history :: History d | eff) Unit
-replaceState = emitStateChange <%> replaceStateRaw
-
-goBack :: forall d eff. Eff (reactive :: Reactive, history :: History d | eff) Unit
-goBack = emitStateChange "back" *> goBackRaw
-
-goForward :: forall d eff. Eff (reactive :: Reactive, history :: History d | eff) Unit
-goForward = emitStateChange "forward" *> goForwardRaw
-
-goState :: forall d eff. Delta -> Eff (reactive :: Reactive, history :: History d | eff) Unit
-goState = emitStateChange' <%> goStateRaw
-  where emitStateChange' x = emitStateChange $ "goState(" ++ show x ++ ")"
-
-subscribeStateChange :: forall a b eff.
-                        (Event a -> Eff (reactive :: Reactive | eff) b) ->
-                        Eff (reactive :: Reactive | eff) Subscription
-subscribeStateChange = subscribeEvented statechange
+foreign import history :: forall e. Window -> Eff (history :: HISTORY | e) History
+foreign import back :: forall e. History -> Eff (history :: HISTORY | e) Unit
+foreign import forward :: forall e. History -> Eff (history :: HISTORY | e) Unit
+foreign import go :: forall e. History -> Delta -> Eff (history :: HISTORY | e) Unit
+foreign import pushState :: forall a e. History -> a -> DocumentTitle -> URL -> Eff (history :: HISTORY | e) Unit
+foreign import replaceState :: forall a e. History -> a -> DocumentTitle -> URL -> Eff (history :: HISTORY | e) Unit
+foreign import state :: forall a e. History -> Eff (history :: HISTORY | e) a
